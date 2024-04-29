279cadebf44e3830a18033661e1d6f4c99ec2646
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-29 21:43:55 +0100

    fix delay

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     """async generator"""
     for _ in range(10):
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
