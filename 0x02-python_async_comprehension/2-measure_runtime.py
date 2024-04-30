7a4128bd1919aa485e2e0700f4b0cdc60ff36e7d
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-30 12:09:54 +0100

    pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,14 +3,16 @@
 
 import asyncio
 import time
+from typing import List
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    start_time = time.time()
-    list_task = []
+    """measure the total runtime and return it"""
+    start_time: float = time.time()
+    list_task: List = []
     for _ in range(4):
         list_task.append(asyncio.create_task(async_comprehension()))
     await asyncio.gather(* list_task)
